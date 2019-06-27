7762578bd0414252d702035689a8afe5afbdd4e4
walid <walid.hamiciwild@gmail.com>
2019-06-27 17:24:54 +0200

    screen acceuil +add font

--- mobile-app/screen/Accueil/accueil.js
+++ mobile-app/screen/Accueil/accueil.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react'
-import { Image, Text, View } from 'react-native'
+import { StyleSheet,TouchableOpacity, Image, Text, View } from 'react-native'
 
 export default class accueil extends Component {
     render() {
         return (
             <View >
                 <View>
-                    <Text style={{ textAlign: 'center', marginTop: 40, fontSize: 40 }}> HandiPaca </Text>
+                this.state.fontLoaded ? (
+                 <Text style={{   fontFamily: 'Comfortaa',textAlign: 'center', marginTop: 40, fontSize: 40 }}> HandiPaca </Text>
+                      ) : null
 
                 </View>
-                <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-                    <Image style={{ resizeMode: 'contain', height: 300, width: 300,marginTop:55 }} source={require('../../assets/Ping.png')} />
-                </View>
-                <View style={{ flexDirection: 'row', justifyContent: 'space-around',marginTop:80 }} >
-                    <Image style={{ resizeMode: 'contain', height: 80, width: 80 }} source={require('../../assets/logopaca.png')} />
-                    <Image style={{ resizeMode: 'contain', height: 80, width: 80 }} source={require('../../assets/dataweek.png')} />
-                    <Image style={{ resizeMode: 'contain', height: 70, width: 70 }} source={require('../../assets/wild.png')} />
+                <TouchableOpacity>
+                    <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+                        <Image style={{ resizeMode: 'contain', height: 300, width: 300, marginTop: 55 }} source={require('../../assets/Ping.png')} />
+                    </View>
+                </TouchableOpacity>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginTop: 80 }} >
+                    <TouchableOpacity>
+                        <Image style={{ resizeMode: 'contain', height: 80, width: 80 }} source={require('../../assets/logopaca.png')} />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Image style={{ resizeMode: 'contain', height: 80, width: 80 }} source={require('../../assets/dataweek.png')} />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Image style={{ resizeMode: 'contain', height: 70, width: 70 }} source={require('../../assets/wild.png')} />
+                    </TouchableOpacity>
                 </View>
             </View>
         )
