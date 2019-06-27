b4ddb619177c56c5205957ecbafb18300c095482
Jean <jeanlabedwcs@gmail.com>
2019-06-27 16:27:18 +0200

    Add footer component

--- mobile-app/Components/footer.js
+++ mobile-app/Components/footer.js
@@ -1,12 +1,22 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
-
-export default class footer extends Component {
-    render() {
-        return (
-            <View>
-                <Text> textInComponent </Text>
-            </View>
-        )
-    }
+import React, { Component } from 'react';
+import { Container, Footer, FooterTab, Button, Icon , Text} from 'native-base';
+export default class Footer extends Component {
+  render() {
+    return (
+       
+        <Container>
+        <Footer>
+          <FooterTab>
+            <Button style= {{backgroundColor: '#a0cbf3'}}>
+              <Text
+              style={{fontSize: 40, color:'black', paddingTop: 30}}
+              >Map</Text>
+            </Button>
+            
+          </FooterTab>
+        </Footer>
+      </Container>
+      
+    );
+  }
 }
